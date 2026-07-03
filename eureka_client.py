ad33ab319a4435fabee2db8cba88c621929b62ac
silentbutmad <mohitmahajan489@gmail.com>
2026-07-03 18:29:42 +0530

    /ocr/ added

--- eureka_client.py
+++ eureka_client.py
@@ -1,26 +1,27 @@
 import os
 import py_eureka_client.eureka_client as eureka_client
 
-HOST = os.getenv(
-    "HOSTNAME",
-    "ocr-dr2b.onrender.com"
-)
+HOST = "ocr-dr2b.onrender.com"
 
-PORT = int(os.getenv("PORT", "8000"))
+PORT = int(os.getenv("PORT", "10000"))
 
-EUREKA_SERVER = os.getenv(
-    "EUREKA_SERVER",
-    "https://admin:admin123@eurekadiscoveryserver-ick0.onrender.com/eureka/"
-)
+EUREKA_SERVER = "https://admin:admin123@eurekadiscoveryserver-ick0.onrender.com/eureka/"
 
 async def register():
     await eureka_client.init_async(
         eureka_server=EUREKA_SERVER,
         app_name="OCR-SERVICE",
+
         instance_host=HOST,
         instance_port=PORT,
-        instance_id=f"{HOST}:{PORT}",
+
         home_page_url=f"https://{HOST}",
         status_page_url=f"https://{HOST}/health",
         health_check_url=f"https://{HOST}/health",
+
+        instance_id=f"OCR-SERVICE:{HOST}",
+
+        # Register as HTTPS
+        secure_port=443,
+        secure_port_enabled=True,
     )
